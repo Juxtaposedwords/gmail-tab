3c21653c9aa2bd213d75041e234f8e6dbc78619d
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2017-02-27 09:40:07 -0800

    tweaked the funny options

--- replacer.js
+++ replacer.js
@@ -1,9 +1,9 @@
 var replacements = {
-  "primary" : [':2w', "Personal", "Test Data"],
-  "social" :  [':2x', "Scheduling", "Messages from social networks, media-sharing sites, online dating services, and other social websites."],
+  "primary" : [':2w', "Personal", "Personal isn't the same as important."],
+  "social" :  [':2x', "Scheduling", "Cause, remember: no matter where you go... there you are."],
   "promotions" : [':2y', "TBD", "Not currently in use"],
   "updates" : [':2z', "TBD", "Not currently in use"],
-  "forums" :  [':30', '/dev/null', "When you have nowhere else to go"]}
+  "forums" :  [':30', '/dev/null', "When you've nowhere else to go"]}
 
 for (var key in replacements) {
   var elm  = document.getElementById(replacements[key][0])
